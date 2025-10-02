2d692c974f6b22d02ff6e7be734ff13b3af9a3d2
PaikSeungHo <tmdgh4922@naver.com>
2025-10-02 12:42:46 +0900

    로그인 및 설정 페이지 구현

---
+++ BenefitMapFrontend/src/styles/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+    color: #333;
+    background-color: #ffffff;
+  }
+`;
+
+export default GlobalStyle;
