49b9993b09057de1b8471cf514de09a94589fa58
SongYuHyeon <songyhyh00@gmail.com>
2025-10-25 20:10:17 +0900

    오류수정2

--- BenefitMapBackend/src/main/java/com/benefitmap/backend/BenefitMapBackendApplication.java
+++ BenefitMapBackend/src/main/java/com/benefitmap/backend/BenefitMapBackendApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BeneFitMapBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BeneFitMapBackendApplication.class, args);
-	}
-
+public class BenefitMapBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BenefitMapBackendApplication.class, args);
+    }
 }
